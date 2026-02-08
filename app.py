4778772696d5a81565955120a968f02453352743
iamvishnub <135424142+iamvishnub@users.noreply.github.com>
2026-02-08 19:26:44 +0530

    Change database URI to use environment variable

--- app.py
+++ app.py
@@ -198,7 +198,9 @@ def inject_user():
 
 
 # Database setup
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+import os
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
